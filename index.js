3072defaabdf32875064ea1215f38aba811c54ca
ka-di-ma-hh <junewanja8@gmail.com>
2024-07-21 01:18:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const tutorials = [
-  "What Does The This Keyword Mean?",
-  "What Is The Constructor OO Pattern?",
- "Implementing Blockchain Web API",
-  'The Test Driven Development Workflow',
-  "What Is NaN And How Can We Check For It",
-   "What Is The Difference Between StopPropagation And PreventDefault?",
-   "Immutable State And Pure Functions",
-  "What Is The Difference Between == And ===?",
-   "What Is The Difference Between Event Capturing And Bubbling?",
-   "What Is JSONP?",
+	"what does the this keyword mean?",
+	"What is the Constructor OO pattern?",
+	"implementing Blockchain Web API",
+	"The Test Driven Development Workflow",
+	"What is NaN and how Can we Check for it",
+	"What is the difference between stopPropagation and preventDefault?",
+	"Immutable State and Pure Functions",
+	"what is the difference between == and ===?",
+	"what is the difference between event capturing and bubbling?",
+	"what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+let test = tutorials.map((element) =>
+	element
+		.split(" ")
+		.map((element) => element.slice(0, 1)[0].toUpperCase() + element.slice(1))
+		.join(" ")
+);
+	return test
+
+};
+
+// console.log(titleCased());
